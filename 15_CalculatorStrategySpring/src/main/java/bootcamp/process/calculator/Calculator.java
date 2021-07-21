eb87e730ca450765f8ebf3242b45f69e56bf1749
Weinan Tan <weinan.tan.2017@live.rhul.ac.uk>
2021-07-21 01:20:14 +0100

    Updated a few things

--- 15_CalculatorStrategySpring/src/main/java/bootcamp/process/calculator/Calculator.java
+++ 15_CalculatorStrategySpring/src/main/java/bootcamp/process/calculator/Calculator.java
@@ -3,20 +3,36 @@ package bootcamp.process.calculator;
 import bootcamp.data.Params;
 import bootcamp.data.Result;
 import bootcamp.process.element.ProcessingElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 
 // TODO this is a Component
+@Component
 public class Calculator {
-    private final ProcessingElement adder = null; //FIXME
-    private final ProcessingElement subtractor = null; //FIXME
-    private final ProcessingElement multiplier = null; //FIXME
-    private final ProcessingElement divider = null; //FIXME
 
-    // TODO Calculator to be autowired to inject ProcessingElements
-    public Calculator(/* TODO Use qualifier to get adder by name */ ProcessingElement adder,
-            /* TODO Use qualifier to get subtractor by name */ ProcessingElement subtractor,
-            /* TODO Use qualifier to get multiplier by name */ ProcessingElement multiplier,
-            /* TODO Use qualifier to get divider by name */ ProcessingElement divider) {
-        // TODO assign and initialise processing elements.
+    @Qualifier("adder")
+    private final ProcessingElement adder;
+    @Qualifier("subtractor")
+    private final ProcessingElement subtractor ;
+    @Qualifier("multiplier")
+    private final ProcessingElement multiplier ;
+    @Qualifier("divider")
+    private final ProcessingElement divider ;
+
+    @Autowired
+    public Calculator(Adder adder,Subtractor subtractor, Multiplier multiplier, Divider divider )
+//            /* TODO Use qualifier to get adder by name */ ProcessingElement adder,
+//            /* TODO Use qualifier to get subtractor by name */ ProcessingElement subtractor,
+//            /* TODO Use qualifier to get multiplier by name */ ProcessingElement multiplier,
+//            /* TODO Use qualifier to get divider by name */ ProcessingElement divider
+    {
+        this.adder = adder;
+        this.subtractor = subtractor;
+        this.multiplier = multiplier;
+        this.divider = divider;
     }
 
 
--- 15_CalculatorStrategySpring/src/main/java/bootcamp/process/element/impl/ElementFactory.java
+++ 15_CalculatorStrategySpring/src/main/java/bootcamp/process/element/impl/ElementFactory.java
@@ -1,38 +1,31 @@
 package bootcamp.process.element.impl;
-
 import bootcamp.process.element.ProcessingElement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 import java.math.BigDecimal;
 
 // TODO this is a configuration class.
 // TODO this needs to scan components in the relevant package(s).
 // TODO factory methods to be annotated as Bean
 
-
-@ComponentScan
 @Configuration
+@ComponentScan
 public class ElementFactory {
-
     @Bean
-    ProcessingElement adder() {
+    public ProcessingElement adder() {
         return BigDecimal::add;
     }
-
     @Bean
-    ProcessingElement subtractor() {
+    public ProcessingElement subtractor() {
         return BigDecimal::subtract;
     }
-
     @Bean
-    ProcessingElement multiplier() {
+    public ProcessingElement multiplier() {
         return BigDecimal::multiply;
     }
-
     @Bean
-    ProcessingElement divider() {
+    public ProcessingElement divider() {
         return BigDecimal::divide;
     }
 }
