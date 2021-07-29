9e353f4b885d15547e6027e45cf322ac565560af
Weinan Tan <weinan.tan.2017@live.rhul.ac.uk>
2021-07-29 15:10:00 +0100

    Updated with a newer Version of Calculator with Map. And Rewrote all the tests and passed.

--- 08_CalculatorStrategyMap/src/test/java/TestCalculatorMap.java
+++ 08_CalculatorStrategyMap/src/test/java/TestCalculatorMap.java
@@ -1,71 +1,57 @@
+import bootcamp.data.Params;
+import bootcamp.data.Result;
+import bootcamp.data.Status;
+import bootcamp.process.calculator.Calculator;
+import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestCalculatorMap {
+    Calculator cal = new Calculator();
+    @Test
+    public void shouldAddTwoParams() {
+        Params params = new Params(new BigDecimal(3),new BigDecimal(2),"+");
+        Result results = cal.calculate(params);
+        assertEquals(results.getValue(),Optional.of(new BigDecimal(5)));
+   }
 
-//    @Test
-//    public void shouldAddTwoParamsddd() throws Exception {
-////        Params params = new Params(new BigDecimal(3),new BigDecimal(2),"+");
-////        ElementFactory factory = new ElementFactory();
-////        var x = factory.create(params.getOperator()).get().process(params.getX(),params.getY());
-////        Calculator cal = new Calculator(factory);
-////        Optional result = cal.calculate(params).getValue();
-////        assertEquals(new Result(Status.SUCCESS,"", Optional.of(new BigDecimal(5))).getValue(),result);
-//   }
-//
-//    @Test
-//    public void shouldAddTwoParams(){
-//        Params params = new Params(new BigDecimal(3),new BigDecimal(2),"+");
-//        Map<String, ProcessingElement> elementMap = new HashMap<>();
-//        MapFactory factory = new MapFactory(elementMap);
-//        factory.create(params.getOperator());
-//        elementMap.get(factory.create(params.getOperator()));
-//        Calculator cal = new Calculator(factory);
-//
-//        Optional result = cal.calculate(params).getValue();
-//        assertEquals(Optional.of(new BigDecimal(5)),result);
-//    }
-//
-//
-//    @Test
-//    public void shouldSubTwoParams(){
-//        Params params = new Params(new BigDecimal(3),new BigDecimal(2),"-");
-//        Map<String, ProcessingElement> elementMap = new HashMap<>();
-//        MapFactory factory = new MapFactory(elementMap);
-//        factory.create(params.getOperator());
-//        elementMap.get(factory.create(params.getOperator()));
-//        Calculator cal = new Calculator(factory);
-//
-//        Optional result = cal.calculate(params).getValue();
-//        assertEquals(Optional.of(new BigDecimal(1)),result);
-//    }
-//
-//    @Test
-//    public void shouldTimesTwoParams(){
-//        Params params = new Params(new BigDecimal(4),new BigDecimal(2),"*");
-//        Map<String, ProcessingElement> elementMap = new HashMap<>();
-//        MapFactory factory = new MapFactory(elementMap);
-//        factory.create(params.getOperator());
-//        elementMap.get(factory.create(params.getOperator()));
-//        Calculator cal = new Calculator(factory);
-//
-//        Optional result = cal.calculate(params).getValue();
-//        assertEquals(Optional.of(new BigDecimal(8)),result);
-//    }
-//
-//    @Test
-//    public void shouldDivideTwoParams(){
-//        Params params = new Params(new BigDecimal(4),new BigDecimal(2),"/");
-//        Map<String, ProcessingElement> elementMap = new HashMap<>();
-//        MapFactory factory = new MapFactory(elementMap);
-//        factory.create(params.getOperator());
-//        elementMap.get(factory.create(params.getOperator()));
-//        Calculator cal = new Calculator(factory);
-//
-//        Optional result = cal.calculate(params).getValue();
-//        assertEquals(Optional.of(new BigDecimal(2)),result);
-//    }
+    @Test
+    public void shouldSubTwoParams(){
+        Params params = new Params(new BigDecimal(3),new BigDecimal(2),"-");
+       Result result = cal.calculate(params);
+        assertEquals(Optional.of(new BigDecimal(1)),result.getValue());
+    }
 
+    @Test
+    public void shouldTimesTwoParams(){
+        Params params = new Params(new BigDecimal(4),new BigDecimal(2),"*");
+       Result result = cal.calculate(params);
+        assertEquals(Optional.of(new BigDecimal(8)),result.getValue());
+    }
 
+    @Test
+    public void shouldDivideTwoParams(){
+        Params params = new Params(new BigDecimal(4),new BigDecimal(2),"/");
+        Result result = cal.calculate(params);
+        assertEquals(result.getValue(),Optional.of(new BigDecimal(2)));
+    }
+
+    @Test
+    public void ShouldReturnStatusInvalidOperation(){
+        Params params = new Params(new BigDecimal(4),new BigDecimal(2),"454");
+        Status status = cal.calculate(params).getStatus();
+        assertEquals(status,Status.INVALID_OPERATION);
+    }
+
+    @Test
+    public void ShouldReturnStatusArithmeticError(){
+        Params params = new Params(new BigDecimal(4),new BigDecimal(0),"/");
+        Status status = cal.calculate(params).getStatus();
+        assertEquals(status,Status.ARITHMETIC_ERROR);
+    }
 
 
 
